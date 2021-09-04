9abc162da163bb18462c486cbc4e00a82a26fa38
Zach <zheylmun@gmail.com>
2021-09-03 22:22:23 -0400

    fix scan command

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,9 +1,1 @@
-sonar-scanner \
-  -Dsonar.projectKey=void_shell \
-  -Dsonar.sources=. \
-  -Dsonar.cfamily.build-wrapper-output=bw-output \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.login=2bdae852ef3f2970a817c54984341dee306ec16f \
-  -Dsonar.cfamily.threads=8 \
-  -Dsonar.cfamily.cache.enabled=true \
-  -Dsonar.cfamily.cache.path=./build/analysis_cache
+sonar-scanner -Dsonar.cfamily.build-wrapper-output=build/bw-output -Dsonar.cfamily.gcov=build/artifacts/gcov/sonar_coverage.xml
