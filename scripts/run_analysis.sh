9d0a5f6b24a54b682fc932a30d69972da038ca53
Zachary Heylmun <zacharyheylmun@Zacharys-Mac-mini.local>
2021-09-03 23:30:36 -0400

    Analysis script update

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,1 +1,7 @@
-sonar-scanner -Dsonar.cfamily.build-wrapper-output=build/bw-output -Dsonar.cfamily.gcov=build/artifacts/gcov/sonar_coverage.xml
+sonar-scanner \
+  -Dsonar.organization=voidstarsolutions \
+  -Dsonar.projectKey=void_shell \
+  -Dsonar.sources=./source/ \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.cfamily.build-wrapper-output=build/bw-output \
+  -Dsonar.cfamily.gcov=build/artifacts/gcov/sonar_coverage.xml
