709a9102ae7e6158a78a5f896c688e64027c1602
Zachary Heylmun <zacharyheylmun@Zacharys-Mac-mini.local>
2021-09-04 00:20:31 -0400

    limit analysis to source

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -1,7 +1,7 @@
 sonar-scanner \
   -Dsonar.organization=voidstarsolutions \
   -Dsonar.projectKey=void_shell \
-  -Dsonar.sources=./ \
+  -Dsonar.sources=./source \
   -Dsonar.host.url=https://sonarcloud.io \
   -Dsonar.cfamily.compile-commands=build/compile_commands.json \
   -Dsonar.cfamily.gcov=build/artifacts/gcov/sonar_coverage.xml
