c9197ea4068dc03a2de5a88917ff4b4ddfbb79f6
Zach <zheylmun@gmail.com>
2021-09-10 20:26:58 -0400

    add key

--- scripts/run_analysis.sh
+++ scripts/run_analysis.sh
@@ -4,5 +4,5 @@ if [[ "$BUILDKITE_PULL_REQUEST" == "false" ]]
 then
 sonar-scanner -Dsonar.branch.name=$BUILDKITE_BRANCH
 else
-sonar-scanner -Dsonar.pullrequest.base=$BUILDKITE_PULL_REQUEST_BASE_BRANCH -Dsonar.pullrequest.branch=$BUILDKITE_BRANCH
+sonar-scanner -Dsonar.pullrequest.base=$BUILDKITE_PULL_REQUEST_BASE_BRANCH -Dsonar.pullrequest.branch=$BUILDKITE_BRANCH -Dsonar.pullrequest.key=$BUILDKITE_PULL_REQUEST
 fi
